6af9a560ae26b4d1f870a3824ec5a25b78b54f4f
TheMasz <one.oi910@gmail.com>
2021-02-15 19:02:32 +0700

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import upload from 'express-fileupload';
-import path from 'path';
+import upload from "express-fileupload";
+import path from "path";
 import productRouter from "./routes/productRouter.js";
 import userRouter from "./routes/userRouter.js";
 import categoryRouter from "./routes/categoriesRouter.js";
 import orderRouter from "./routes/orderRouter.js";
 import uploadRouter from "./routes/uploadRouter.js";
 
-
 dotenv.config();
 
 const app = express();
 
-
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 app.use(upload());
 
 app.use(express.json());
@@ -31,18 +30,18 @@ mongoose
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log(err));
 
-app.use('/api/uploads', uploadRouter);
+app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/categories", categoryRouter);
 app.use("/api/orders", orderRouter);
 
-
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
+app.get("/", (req, res) => {
+  res.send("server ready");
+});
 
 const port = process.env.PORT || 5000;
 
